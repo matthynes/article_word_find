05d6c7e4734e094a009c8fabb84d290e4f95bb35
Matthew Hynes <matthew.hynes@mun.ca>
2016-07-23 18:27:58 -0230

    Change word weight to double and stop program from closing upon finish

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
+#!C:\Python27\python.exe
+
 import os
+import re
 
-import PyPDF2
+from pyth.plugins.rtf15.reader import Rtf15Reader
+from pyth.plugins.xhtml.writer import XHTMLWriter
+from dehtml import dehtml
 
-root = os.path.dirname(os.path.dirname(__file__))
+root = '.'
 
-for subdir, dirs, files in os.walk(root):
-    for file in files:
-        path = subdir + os.sep + file
+input = raw_input(
+    "Input the word you want to search, followed by a comma and then its weight, then hit enter. ex; fish, 3. Enter ; when finished.\n")
 
-        if path.endswith(".py"):
-            print(path)
+words = ()
+
+while input != ';':
+    i_list = input.split(', ')
+    words += ([i_list[0], i_list[1]],)
+    input = raw_input("Another word? ; to quit\n")
+
+scores = {w: float(n) for w, n in words}
+total = 0
+
+output = open("results.txt", "w")
+
+results = {}
+
+for dirs, subdirs, files in os.walk(root):
+    for f in files:
+        if f.endswith('.rtf'):
+            doc = Rtf15Reader.read(open(f, "rb"))
+            total = 0
+            text = dehtml(XHTMLWriter.write(doc).read().lower().split())
+            for word in text:
+                word = re.sub('\W+', '', word)
+                total += scores.get(word, 0)
+
+            results[f] = total
+
+for key, value in sorted(results.items()):
+    output.write(key + "       " + str(value) + "\n")
+
+print "Finished! Check results.txt"
+raw_input("\nPress enter to close.")
